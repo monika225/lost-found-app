feb4a61874899e5f739dd0c130ded050f85b1a43
Monika D <monikadhanagopal@gmail.com>
2026-05-01 09:21:35 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ function showLost() {
   let output = "<h2>Lost Items</h2>";
 
   db.collection("lost").get().then(snapshot => {
+
     snapshot.forEach(doc => {
       let i = doc.data();
+      let color = "#ff6b6b";
       output += `
         <div style="background:white;padding:10px;margin:10px;border-radius:5px;">
           <b>${i.name}</b><br>
@@ -22,8 +24,14 @@ function showFound() {
   let output = "<h2>Found Items</h2>";
 
   db.collection("found").get().then(snapshot => {
+
+    if(snapshot.empty){
+  output += "<p>No items found 😔</p>";
+}
+
     snapshot.forEach(doc => {
       let i = doc.data();
+      let color = "#51cf66";
       output += `
         <div style="background:white;padding:10px;margin:10px;border-radius:5px;">
           <b>${i.name}</b><br>
@@ -39,15 +47,21 @@ function showFound() {
 }
 
 function searchItems() {
-  const query = document.getElementById("search").value.toLowerCase();
+  const query = document.getElementById("search").value.trim().toLowerCase();
+
   let output = "<h2>Search Results</h2>";
+  let found = false;
 
   db.collection("lost").get().then(snapshot => {
+
     snapshot.forEach(doc => {
       let i = doc.data();
-      if (i.name.toLowerCase().includes(query)) {
+
+      if(i.name && i.name.toLowerCase().includes(query)) {
+        found = true;
+
         output += `
-          <div style="background:white;padding:10px;margin:10px;border-radius:5px;">
+          <div class="card">
             <b>${i.name}</b><br>
             📍 ${i.location} (Lost)
           </div>
@@ -56,11 +70,15 @@ function searchItems() {
     });
 
     db.collection("found").get().then(snapshot2 => {
+
       snapshot2.forEach(doc => {
         let i = doc.data();
-        if (i.name.toLowerCase().includes(query)) {
+
+        if(i.name && i.name.toLowerCase().includes(query)) {
+          found = true;
+
           output += `
-            <div style="background:white;padding:10px;margin:10px;border-radius:5px;">
+            <div class="card">
               <b>${i.name}</b><br>
               📍 ${i.location} (Found)
             </div>
@@ -68,7 +86,12 @@ function searchItems() {
         }
       });
 
+      if(!found){
+        output += "<p>No items found 😔</p>";
+      }
+
       document.getElementById("items").innerHTML = output;
     });
+
   });
 }
